fcc8c390189090fa34352bc811e764d99f5dd97d
mike <michaelwilsonsj@hotmail.com>
2021-08-17 17:42:17 -0400

    clone for upload

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RouterModule } from "@angular/router";
+import { IvyCarouselModule } from "angular-responsive-carousel";
+
+import { HomeComponent } from "./home.component";
+
+import { SectionsModule } from "../sections/sections.module";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    SectionsModule,
+    IvyCarouselModule,
+  ],
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
+  providers: [],
+})
+export class HomeModule {}
